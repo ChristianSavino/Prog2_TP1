f2cabce390ad68f3930aec651cf255e42dcc9eaf
Fernando Leonel Moschen <64292259+ferchumoschen@users.noreply.github.com>
2022-08-28 23:34:17 -0300

    Chau 0

--- TP1_IntroduccionAndroid_Grupo4/app/src/main/java/com/example/tp1_introduccionandroid_grupo4/Calculator.java
+++ TP1_IntroduccionAndroid_Grupo4/app/src/main/java/com/example/tp1_introduccionandroid_grupo4/Calculator.java
@@ -11,109 +11,6 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Calculator extends AppCompatActivity {
-   /*
-    private TextView operationTextView;
-    private TextView secondOperationTypeTextView;
-
-    //1 Sumar, 2 Restar, 3 Multiplicar y 4 Dividir
-    private int operationType;
-    private int num1;
-    private int num2;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        this.setTitle("Calculadora");
-        setContentView(R.layout.activity_calculator);
-
-        //operationTypeTextView = (TextView) findViewById(R.id.operationTypeTextView);
-        //secondOperationTypeTextView = (TextView) findViewById(R.id.secondOperationTypeTextView);
-    }
-
-    public void SetAdd(View view) {
-        PreCalculate(view, operationTextView.getText().toString(),1);
-    }
-
-    public void SetSubtract(View view) {
-        PreCalculate(view, operationTextView.getText().toString(),2);
-    }
-
-    public void SetMultiply(View view) {
-        PreCalculate(view, operationTextView.getText().toString(),3);
-    }
-
-    public void SetDivide(View view) {
-        PreCalculate(view, operationTextView.getText().toString(),4);
-    }
-
-    public void One(View view) {
-        AddNumber("1");
-    }
-
-    public void Back(View view){
-        Intent volver = new Intent(this, MainActivity.class);
-        startActivity(volver);
-    }
-
-    public void Delete(View view)
-    {
-        operationTextView.setText("0");
-        secondOperationTypeTextView.setText("");
-        operationType = 0;
-    }
-
-    void AddNumber(String num) {
-        operationTextView.setText(operationTextView.getText().toString() + num);
-    }
-
-    void PreCalculate(View view,String num, int opType) {
-        if (num1 == 0) {
-            num1 = Integer.parseInt(num);
-            secondOperationTypeTextView.setText(num + " + ");
-            operationTextView.setText("0");
-            operationType = opType;
-        }
-        else {
-            num2 = Integer.parseInt(num);
-            secondOperationTypeTextView.setText(secondOperationTypeTextView.getText().toString() + num);
-            Calculate(view);
-            PreCalculate(view,operationTextView.getText().toString(),opType);
-        }
-
-    }
-
-    public void Calculate(View view) {
-        if(num1 != 0 && num2 != 0 && operationType != 0) {
-            String result = "";
-
-            switch (operationType) {
-                case 1:
-                    result = Sumar(num1,num2);
-                    break;
-                case 2:
-                    result = Restar(num1,num2);
-                    break;
-                case 3:
-                    result = Multiplicar(num1,num2);
-                    break;
-                case 4:
-                    result = Dividir(num1,num2);
-
-                default:
-                    //aca mostrar alguna especie de error/ excepcion
-            }
-
-            operationTextView.setText(result);
-            num1 = 0;
-            num2 = 0;
-            operationType = 0;
-        }
-    }
-
-    String Sumar(int n1, int n2) {return Integer.toString(n1 + n2);}
-    String Restar(int n1, int n2) {return Integer.toString(n1 - n2);}
-    String Multiplicar(int n1,int n2){return Integer.toString(n1 * n2);}
-    String Dividir(int n1,int n2){return Integer.toString(n1 / n2);}*/
 
     private int num1 = 0, num2 = 0;
     private TextView pantalla;
@@ -144,6 +41,14 @@ public class Calculator extends AppCompatActivity {
         btnBorrar = (Button) findViewById(R.id.btnBorrar);
 
         pantalla = (TextView) findViewById(R.id.tv1);
+        pantalla.setText(null);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pantalla.setText(pantalla.getText() + "1");
+            }
+        });
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -221,7 +126,7 @@ public class Calculator extends AppCompatActivity {
                 if (pantalla.getText().length() != 0) {
                     num1 = Integer.parseInt(pantalla.getText() + "");
                     Sumar = true;
-                    pantalla.setText(num1 +"+");
+                    pantalla.setText(null);
                 }
             }
         });
@@ -232,7 +137,7 @@ public class Calculator extends AppCompatActivity {
                 if (pantalla.getText().length() != 0) {
                     num1 = Integer.parseInt(pantalla.getText() + "");
                     Restar = true;
-                    pantalla.setText(num1 +"-");
+                    pantalla.setText(null);
                 }
             }
         });
@@ -243,7 +148,7 @@ public class Calculator extends AppCompatActivity {
                 if (pantalla.getText().length() != 0) {
                     num1 = Integer.parseInt(pantalla.getText() + "");
                     Multiplicar = true;
-                    pantalla.setText(num1 +"x");
+                    pantalla.setText(null);
                 }
             }
         });
@@ -252,9 +157,10 @@ public class Calculator extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (pantalla.getText().length() != 0) {
-                    num1 = Integer.parseInt(pantalla.getText() + "");
+                    num1 = Integer.parseInt(pantalla.getText().toString() + "");
                     Dividir = true;
-                    pantalla.setText(num1 +"/");
+
+                    pantalla.setText(null);
                 }
             }
         });
@@ -263,7 +169,8 @@ public class Calculator extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (Sumar || Restar || Multiplicar || Dividir) {
-                    num2 = Integer.parseInt(pantalla.getText() + "");
+                    num2 = Integer.parseInt(pantalla.getText().toString() + "");
+
                 }
 
                 if (Sumar) {
@@ -289,13 +196,13 @@ public class Calculator extends AppCompatActivity {
                 }
 
             }
-
         });
+
 
         btnBorrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pantalla.setText("0");
+                pantalla.setText(null);
                 num1 = 0;
                 num2 = 0;
             }
@@ -307,6 +214,7 @@ public class Calculator extends AppCompatActivity {
         if(pantalla.getText() == "0")
             pantalla.setText(n);
         else
-            pantalla.setText( pantalla.getText() + n);
+            pantalla.setText(pantalla.getText() + n);
+
     }
 }
